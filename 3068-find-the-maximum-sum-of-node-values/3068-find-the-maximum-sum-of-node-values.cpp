e25228757adab30e6a5f18dee82f6c8843266845
RAVINDRAN-S <166739819+RAVINDRAN-S@users.noreply.github.com>
2025-05-23 16:39:58 +0530

    Time: 7 ms (43.87%), Space: 131.9 MB (30.32%) - LeetHub

---
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -0,0 +1,39 @@
+class Solution 
+{
+public:
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) 
+    {
+       
+        long long sum = 0;
+        vector<int> gains;
+
+     
+        for (int num : nums) 
+        {
+            int xorVal = num^k;
+            int gain = xorVal - num;
+            sum += num;
+            gains.push_back(gain);
+        }
+
+       
+        sort(gains.rbegin(), gains.rend());
+
+       
+        long long maxGain = 0, currentGain = 0;
+        int count = 0;
+
+        for (int g : gains) {
+            currentGain += g;
+            count++;
+        
+            if (count % 2 == 0) 
+            {
+                maxGain = max(maxGain, currentGain);
+            }
+        }
+
+        
+        return sum + maxGain;
+    }
+};
