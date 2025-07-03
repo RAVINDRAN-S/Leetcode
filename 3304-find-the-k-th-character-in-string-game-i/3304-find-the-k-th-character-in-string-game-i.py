cd1c81f7a8858fd275d3bef9876c788bf39e3366
RAVINDRAN S <166739819+RAVINDRAN-S@users.noreply.github.com>
2025-07-03 12:23:42 +0530

    Time: 34 ms (10%), Space: 11.4 MB (100%) - LeetHub

---
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def kthCharacter(self, k):
+        """
+        :type k: int
+        :rtype: str
+        """
+        s="a"
+        while len(s)<k:
+            for i in s:
+                s+=(chr(ord(i)+1))
+        return s[k-1]
